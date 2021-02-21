080f4724bf8088811a1e2421c60d79faca77d344
Danilo Silva <danilo.silva@msn.com>
2021-02-21 12:16:51 -0300

    Exemplo - Usando Math

---
+++ fundamentos/07-math.js
@@ -0,0 +1,9 @@
+const raio = 5.6;
+// PI é uma constante dentro de Math
+// Pow é uma função de exponenciação (numero, valor elevado)
+const area = Math.PI / Math.pow(raio, 2)
+
+console.log(area);
+
+// Math é um objeto
+console.log(typeof Math);
