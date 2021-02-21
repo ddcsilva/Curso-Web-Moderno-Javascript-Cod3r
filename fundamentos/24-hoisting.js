c645a35b42e7f2b7dc661f6fa8d4f23c4c7e84ce
Danilo Silva <danilo.silva@msn.com>
2021-02-21 17:53:21 -0300

    Exemplo - Entendendo o Hoisting

---
+++ fundamentos/24-hoisting.js
@@ -0,0 +1,20 @@
+console.log('a =', a);
+var a = 2;
+console.log('a =', a);
+
+// Mesmo que:
+
+var a;
+console.log('a =', a);
+a = 2;
+console.log('a =', a);
+
+// Na Function, o efeito seria o mesmo
+
+function teste() {
+    console.log('a =', a);
+    var a = 2;
+    console.log('a =', a);
+}
+
+teste();
