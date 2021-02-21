7ecedc9f1a087418b1f1d238150fcdadb86b846f
Danilo Silva <danilo.silva@msn.com>
2021-02-21 19:20:00 -0300

    Exemplo - Par Nome/Valor

---
+++ fundamentos/26-par_nome_valor.js
@@ -0,0 +1,22 @@
+// Conjunto Par Nome/Valor
+const saudacao = 'Opa' // Contexto Léxico 1
+
+function exec() {
+    const saudacao = 'Falaaa' // Contexto Léxico 2
+    return saudacao
+}
+
+// Objetos são grupos aninhados de pares nome/valor
+const cliente = {
+    nome: 'Pedro',
+    idade: 32,
+    peso: 90,
+    endereco: {
+        logradouro: 'Rua Muito Legal',
+        numero: 123
+    }
+}
+
+console.log(saudacao)
+console.log(exec())
+console.log(cliente)
