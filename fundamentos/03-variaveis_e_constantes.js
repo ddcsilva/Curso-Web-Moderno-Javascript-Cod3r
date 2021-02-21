bcd8fa940da612b01eabddac0a2994be0007be6d
Danilo Silva <danilo.silva@msn.com>
2021-02-21 11:41:27 -0300

    Exemplo - O Básico de Var, Let e Const

---
+++ fundamentos/03-variaveis_e_constantes.js
@@ -0,0 +1,13 @@
+// Declaração de variável
+var a = 3;
+// Declaração de Variável
+let b = 4;
+
+var a = 30;
+// let b = 40; -> Irá gerar um erro
+
+console.log(a, b);
+
+const c = 5;
+// c = 50; -- Irá gerar um erro
+console.log(c);
