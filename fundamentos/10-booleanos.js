bf1c571b2e1532beaecf7ebc2c3d1726f7cf20ab
Danilo Silva <danilo.silva@msn.com>
2021-02-21 13:22:46 -0300

    Exemplo - Tipos em JavaScript: Boolean

---
+++ fundamentos/10-booleanos.js
@@ -0,0 +1,34 @@
+let isAtivo = false
+console.log(isAtivo)
+
+isAtivo = true
+console.log(isAtivo)
+
+isAtivo = 1
+console.log(!!isAtivo)
+
+// Retorno: True
+console.log(!!3)
+console.log(!!-1)
+console.log(!!' ')
+console.log(!!'texto')
+console.log(!![])
+console.log(!!{})
+console.log(!!Infinity)
+console.log(!!(isAtivo = true))
+
+// Retorna: False
+console.log('os falsos...')
+console.log(!!0)
+console.log(!!'')
+console.log(!!null)
+console.log(!!NaN)
+console.log(!!undefined)
+console.log(!!(isAtivo = false))
+
+// Se pelo menos uma expressão for verdadeira, então retorna True
+console.log(!!('' || null || 0 || ' '))
+
+// Imprime o valor que é True
+let nome = 'Lucas'
+console.log(nome || 'Desconhecido')
