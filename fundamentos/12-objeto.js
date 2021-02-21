e0abdde586dc601352c4bbbab45a7cd2e70c0fe3
Danilo Silva <danilo.silva@msn.com>
2021-02-21 16:14:20 -0300

    Exemplo - Tipos em JavaScript: Object

---
+++ fundamentos/12-objeto.js
@@ -0,0 +1,16 @@
+
+// Objeto é uma coleção de pares { Chave: Valor }
+const produto = {};
+produto.nome = 'Celular Ultra Mega';
+produto.preco = 4998.90;
+
+// produto['Desconto Legal'] = 0.40 -> Evitar a utilização de atributos com espaço
+
+console.log(produto);
+
+const produto2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90
+};
+
+console.log(produto2);
