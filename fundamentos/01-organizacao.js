b814a9ab10e1ccc65a449e7d13dce8e0756b3a5f
Danilo Silva <danilo.silva@msn.com>
2021-02-21 11:26:26 -0300

    Exemplo - Organização Básica de um Código JS

---
+++ fundamentos/01-organizacao.js
@@ -0,0 +1,10 @@
+// A sentença pode ou não ocupar somente uma linha. Não pode quebrar uma String
+console.log("Sentença de código");
+
+// Bloco de código
+{
+    {
+        console.log("Olá");
+        console.log('Mundo!') // Padrão do curso
+    }
+}
